bf40f26b8c9e7896f133dbdb6aa31cbfedea3011
Cristian096 <cristian.manzano@pragma.com.co>
2025-01-24 00:33:48 -0500

    feat: add HealthController

---
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/HealthCheckController.java
@@ -0,0 +1,17 @@
+package co.com.bancolombia.api;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/health")
+public class HealthCheckController {
+
+    @GetMapping
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("OK");
+    }
+}
